9d9fbaec385727c5add2622e0d4359f6c2619ccf
Yaoyu Hu <yyhu_live@outlook.com>
2019-02-11 21:47:51 -0500

    Before change directory layout.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='WorkFlow',
+      version='0.1',
+      description='Navie grid map implementation for reinforcement learning training.',
+      author='Yaoyu Hu',
+      author_email='yyhu_live@outlook.com',
+      url='huyaoyu.com',
+      packages=['GridMap', 'EnvInterfaces', 'LineIntersection2D']
+     )
